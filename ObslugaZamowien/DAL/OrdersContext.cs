4c0a5b19233de70b5e6418da6607a2127c7875ea
toome <toome@LAPTOP-S683NICJ>
2019-03-28 21:44:04 +0100

    NUnit, Entity, Swagger, CodeFirst

---
+++ ObslugaZamowien/DAL/OrdersContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObslugaZamowien.DAL
+{
+    public class OrdersContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+    }
+}
